52cafcfe0877d640ac4cc1448d868e1345eedb78
gabibranco <gabi@tatango.com>
2021-04-29 12:47:30 -0400

    mobile only

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-go');
   const logo   = document.querySelector('#logo');
+  const firstP = document.querySelector('#first-p');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 40 && window.scrollY < 177) {
+      if (window.scrollY >= 36 && window.scrollY < 177) {
         navbar.classList.add('navbar-gray');
         logo.classList.add('img-untwist');
-      } else if (window.scrollY >= 177) {
+        logo.classList.add('logo-small');
+        firstP.classList.remove('first-p');
+        firstP.classList.add('content-p');
+        navbar.classList.remove('navbar-red');
+      } else if (window.scrollY >= 177 && window.scrollY < 415) {
         navbar.classList.add('navbar-red');
         navbar.classList.remove('navbar-gray');
+        navbar.classList.remove('navbar-black');
+        logo.classList.remove('img-untwist-black');
+        logo.classList.add('img-untwist');
+      } else if (window.scrollY >= 415) {
+        navbar.classList.remove('navbar-red');
+        navbar.classList.add('navbar-black');
+        logo.classList.remove('img-untwist');
+        logo.classList.add('img-untwist-black');
       }
     });
   }
