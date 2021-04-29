33e402f850ab3670d557f68673e64cca403ee5d5
gabibranco <gabi@tatango.com>
2021-04-29 18:06:23 -0400

    full synch on scrolling

--- index.js
+++ index.js
@@ -4,24 +4,71 @@ const initUpdateNavbarOnScroll = () => {
   const firstP = document.querySelector('#first-p');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 36 && window.scrollY < 142) {
+      if (window.scrollY >= 36 && window.scrollY < 210) {
         navbar.classList.add('navbar-grey');
         logo.classList.add('img-untwist');
         logo.classList.add('logo-small');
         firstP.classList.remove('first-p');
         firstP.classList.add('content-p');
         navbar.classList.remove('navbar-red');
-      } else if (window.scrollY >= 142 && window.scrollY < 415) {
+      } else if (window.scrollY >= 210 && window.scrollY < 500) {
         navbar.classList.add('navbar-red');
-        navbar.classList.remove('navbar-grey');
         navbar.classList.remove('navbar-black');
         logo.classList.remove('img-untwist-black');
         logo.classList.add('img-untwist');
-      } else if (window.scrollY >= 415) {
+      } else if (window.scrollY >= 500 && window.scrollY < 711) {
         navbar.classList.remove('navbar-red');
         navbar.classList.add('navbar-black');
         logo.classList.remove('img-untwist');
         logo.classList.add('img-untwist-black');
+        navbar.classList.remove('navbar-yellow');
+      } else if (window.scrollY >= 711 && window.scrollY < 940) {
+        navbar.classList.add('navbar-yellow');
+        navbar.classList.remove('navbar-black');
+        logo.classList.remove('img-untwist-black');
+        logo.classList.add('img-untwist');
+        navbar.classList.remove('navbar-blue');
+      } else if (window.scrollY >= 940 && window.scrollY < 1134) {
+        navbar.classList.remove('navbar-yellow');
+        navbar.classList.add('navbar-blue');
+        logo.classList.remove('img-untwist');
+        logo.classList.add('img-untwist-black');
+        logo.classList.remove('navbar-grey');
+      }  else if (window.scrollY >= 1134 && window.scrollY < 1339) {
+        navbar.classList.add('navbar-grey');
+        navbar.classList.remove('navbar-blue');
+        logo.classList.remove('img-untwist-black');
+        logo.classList.add('img-untwist');
+        navbar.classList.remove('navbar-red');
+      } else if (window.scrollY >= 1339 && window.scrollY < 1560) {
+        navbar.classList.add('navbar-red');
+        navbar.classList.remove('navbar-black');
+        navbar.classList.remove('navbar-grey');
+        logo.classList.remove('img-untwist-black');
+        logo.classList.add('img-untwist');
+      }  else if (window.scrollY >= 1560 && window.scrollY < 1773) {
+        navbar.classList.remove('navbar-red');
+        navbar.classList.add('navbar-black');
+        logo.classList.remove('img-untwist');
+        logo.classList.add('img-untwist-black');
+        navbar.classList.remove('navbar-yellow');
+      } else if (window.scrollY >= 1773 && window.scrollY < 1977) {
+        navbar.classList.add('navbar-yellow');
+        navbar.classList.remove('navbar-black');
+        logo.classList.remove('img-untwist-black');
+        logo.classList.add('img-untwist');
+        navbar.classList.remove('navbar-grey');
+      } else if (window.scrollY >= 1977 && window.scrollY < 2190) {
+        navbar.classList.add('navbar-grey');
+        navbar.classList.remove('navbar-yellow');
+        logo.classList.remove('img-untwist-black');
+        logo.classList.add('img-untwist');
+        navbar.classList.remove('navbar-red');
+      } else if (window.scrollY >= 2190) {
+        navbar.classList.add('navbar-red');
+        navbar.classList.remove('navbar-grey');
+        logo.classList.remove('img-untwist-black');
+        logo.classList.add('img-untwist');
       }
     });
   }
